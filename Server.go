d75b6c6cd0fb9a132c3af9f3da1f422dc763ecd2
Azzuwan Aziz <azzuwan@gmail.com>
2016-10-14 12:52:00 +0800

    Connection to RethinkDB

--- Server.go
+++ Server.go
@@ -3,26 +3,56 @@ package main
 import(
   "github.com/kataras/iris"
   "github.com/kataras/go-template/html"
+  rdb "gopkg.in/dancannon/gorethink.v2"
+  "fmt"
 ) 
 
 func main(){
-  //api := iris.New()
+  //Database
+  session, err := rdb.Connect(rdb.ConnectOpts{
+      Address: "localhost:28015",
+      Database: "ERN",
+    })
 
+  if err !=nil {
+    fmt.Println(err)
+  }
+
+  res,err1 := rdb.DB("ERN").Table("alerts").Run(session)
+  if err1 != nil {
+    fmt.Println(err)
+  }
+
+  var rows []interface{}
+  err2 := res.All(&rows)
+
+  if err2 !=nil {
+    fmt.Println(err)
+  }
+
+  for _, row := range rows{
+    fmt.Printf("%v",row)
+  }
+
+
+  
   //Configuration
   iris.Static("/public", "./public", 1)
   iris.Static("/data", "./data", 1)
   iris.Config.IsDevelopment = true
-  iris.UseTemplate(html.New(html.Config{
-        Layout: "layout.html",
-    })).Directory("./templates", ".html")
-  
+  iris.UseTemplate(html.New(html.Config{ Layout: "layout.html"})).Directory("./templates", ".html")
+  iris.Config.Websocket.Endpoint = "/sock"
 
   //Routes
   iris.Get("/", index)
-  iris.Get("/node", node)
+  iris.Get("/node", node)  
   iris.Get("/video", video)
   iris.Get("/test/video", downloadVideo)
-  iris.Get("/stream", stream)  
+  iris.Get("/stream", stream)
+
+  //Websocket
+  iris.Websocket.OnConnection( onConnection)
+
   iris.Listen("0.0.0.0:9292")
 }
 
@@ -48,4 +78,18 @@ func stream(ctx *iris.Context){
   if err != nil {
     println(err.Error())
   }
+}
+
+func onConnection(c iris.WebsocketConnection){
+  channel := "ern-channel"
+  c.Join(channel)
+  c.On("monitor", func(message string) {
+      // all connections which are inside this room will receive this message
+    fmt.Println("Message from websocket client! ")
+      //c.To(channel).Emit("chat", "From: "+c.ID()+": "+message)
+  })
+
+  c.OnDisconnect(func() {
+      fmt.Printf("\nConnection with ID: %s has been disconnected!", c.ID())
+  })
 }
